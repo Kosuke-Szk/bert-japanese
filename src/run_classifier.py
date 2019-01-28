f4e86686b32f7e90d2920f4de37534ca386172bd
Kosuke-Szk <brave.ksk@gmail.com>
2019-01-28 16:37:38 +0900

    change classifier for his

--- src/run_classifier.py
+++ src/run_classifier.py
@@ -248,6 +248,42 @@ class LivedoorProcessor(DataProcessor):
             InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
     return examples
 
+class HISProcessor(DataProcessor):
+  """Processor for the livedoor data set (see https://www.rondhuit.com/download.html)."""
+
+  def get_train_examples(self, data_dir):
+    """See base class."""
+    return self._create_examples(
+        self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
+
+  def get_dev_examples(self, data_dir):
+    """See base class."""
+    return self._create_examples(
+        self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
+
+  def get_test_examples(self, data_dir):
+    """See base class."""
+    return self._create_examples(
+        self._read_tsv(os.path.join(data_dir, "test.tsv")), "test")
+
+  def get_labels(self):
+    """See base class."""
+    return list(range(537))
+
+  def _create_examples(self, lines, set_type):
+    """Creates examples for the training and dev sets."""
+    examples = []
+    for (i, line) in enumerate(lines):
+      if i == 0:
+        idx_text = line.index('text')
+        idx_label = line.index('label')
+      else:
+        guid = "%s-%s" % (set_type, i)
+        text_a = tokenization.convert_to_unicode(line[idx_text])
+        label = tokenization.convert_to_unicode(line[idx_label])
+        examples.append(
+            InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
+    return examples
 
 def convert_single_example(ex_index, example, label_list, max_seq_length,
                            tokenizer):
@@ -659,7 +695,8 @@ def main(_):
   tf.logging.set_verbosity(tf.logging.INFO)
 
   processors = {
-      "livedoor": LivedoorProcessor,
+      # "livedoor": LivedoorProcessor,
+    "his": HISProcessor,
   }
 
   tokenization.validate_case_matches_checkpoint(FLAGS.do_lower_case,
